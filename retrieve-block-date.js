29790a32fc5e3c88c9de91b882e96f43cd2a999a
fahdaguenouz <faguenouz@gmail.com>
2026-07-13 11:59:57 -0400

    get date

--- retrieve-block-date.js
+++ retrieve-block-date.js
@@ -1,23 +1,48 @@
-const axios = require("axios");
+const http = require("http");
 
-async function rpc(method, params = []) {
-  const { data } = await axios.post(
-    "http://localhost:18443",
-    {
+function rpc(method, params = []) {
+  return new Promise((resolve, reject) => {
+    const body = JSON.stringify({
       jsonrpc: "1.0",
       id: "1",
       method,
       params,
-    },
-    {
-      auth: {
-        username: "leeloo",
-        password: "multipass",
-      },
-    }
-  );
+    });
 
-  return data.result;
+    const req = http.request(
+      {
+        hostname: "localhost",
+        port: 18443,
+        method: "POST",
+        auth: "leeloo:multipass",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(body),
+        },
+      },
+      (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          const response = JSON.parse(data);
+
+          if (response.error) {
+            reject(response.error);
+          } else {
+            resolve(response.result);
+          }
+        });
+      }
+    );
+
+    req.on("error", reject);
+    req.write(body);
+    req.end();
+  });
 }
 
 async function retrieveBlockDate(height) {
