b4a55551b139430bc8e6c874cb7c1e0f195e24da
fahdaguenouz <faguenouz@gmail.com>
2026-07-15 09:12:54 -0400

    hash

--- hash-160.js
+++ hash-160.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 
 function hash160(input) {
-  const ripemd160 = crypto.createHash('ripemd160').update(input).digest();
-  const sha256 = crypto.createHash('sha256').update(ripemd160).digest();
-  return sha256;
+  const sha = crypto.createHash('sha256').update(input, 'utf8').digest();
+  return crypto.createHash('ripemd160').update(sha).digest();
 }
