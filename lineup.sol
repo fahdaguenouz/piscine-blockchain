d0c8c2c1e416434c5cf7786b6c768a5813d4c073
fahdaguenouz <faguenouz@gmail.com>
2026-07-16 05:48:54 -0400

    lineup

---
+++ lineup.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.4;
+
+contract Lineup {
+    string[] private artists;
+
+    function addArtist(string memory artistName) public {
+        artists.push(artistName);
+    }
+
+    function lineup(uint index) public view returns (string memory) {
+        return artists[index];
+    }
+}
