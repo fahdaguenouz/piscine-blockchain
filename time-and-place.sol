6e97fe4406337056a27ae672c21da3ad4357ec6f
fahdaguenouz <faguenouz@gmail.com>
2026-07-16 05:48:22 -0400

    time

---
+++ time-and-place.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.4;
+
+contract TimeAndPlace {
+    uint private startTime;
+    string private place;
+
+    constructor(uint _startTime, string memory _place) {
+        startTime = _startTime;
+        place = _place;
+    }
+
+    function getStartTime() public view returns (uint) {
+        return startTime;
+    }
+
+    function getPlace() public view returns (string memory) {
+        return place;
+    }
+}
