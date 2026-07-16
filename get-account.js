d78c393f17e9ba537ab492946d165621fd927f44
fahdaguenouz <faguenouz@gmail.com>
2026-07-16 11:40:21 -0400

    getaccount

--- get-account.js
+++ get-account.js
@@ -1,11 +1,13 @@
 const { ethers } = require("ethers");
 
 async function getAccount() {
-  const provider = new ethers.JsonRpcProvider("http://localhost:8545");
+  const provider = new ethers.providers.JsonRpcProvider(
+    "http://localhost:8545"
+  );
 
   const accounts = await provider.listAccounts();
 
-  return accounts[0].address;
+  return accounts[0];
 }
 
 module.exports = getAccount;
