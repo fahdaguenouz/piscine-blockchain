a8bc4d671c6dcc15efeaf0c42ade8dcbbd998443
fahdaguenouz <faguenouz@gmail.com>
2026-07-20 12:07:30 -0400

    toksale

---
+++ token-sale.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./MinimalToken.sol";
+
+contract TokenSale {
+    MinimalToken public token;
+    uint public tokenPrice;
+    address public owner;
+
+    constructor(address tokenAddress, uint price) {
+        require(price > 0, "Price must be greater than 0");
+
+        token = MinimalToken(tokenAddress);
+        tokenPrice = price;
+        owner = msg.sender;
+    }
+
+    function buy() public payable {
+        require(msg.value > 0, "Send some Ether");
+
+        uint amount = msg.value / tokenPrice;
+        require(amount > 0, "Not enough Ether");
+
+        require(
+            token.balanceOf(address(this)) >= amount,
+            "Not enough tokens available"
+        );
+
+        token.transfer(msg.sender, amount);
+    }
+
+    function getPrice() public view returns (uint) {
+        return tokenPrice;
+    }
+
+    function collect() public {
+        require(msg.sender == owner, "Only owner");
+
+        payable(owner).transfer(address(this).balance);
+    }
+}
