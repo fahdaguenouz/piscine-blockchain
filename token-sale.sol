14a301a39f81f9a7530a84a675048bd38cdcd17c
fahdaguenouz <faguenouz@gmail.com>
2026-07-20 13:02:49 -0400

    toksale

--- token-sale.sol
+++ token-sale.sol
@@ -1,27 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface IMinimalToken {
-    function balanceOf(address account) external view returns (uint);
-    function transfer(address to, uint amount) external;
+contract MinimalToken {
+    uint public totalSupply;
+    mapping(address => uint) public balances;
+
+    constructor(uint initialSupply) {
+        require(initialSupply > 0, "Initial supply must be greater than 0");
+        totalSupply = initialSupply;
+        balances[msg.sender] = initialSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint) {
+        return balances[account];
+    }
+
+    function transfer(address to, uint amount) public {
+        require(balances[msg.sender] >= amount, "Insufficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
 }
 
 contract TokenSale {
-    IMinimalToken public token;
+    MinimalToken public token;
     uint public tokenPrice;
     address public owner;
 
     constructor(address tokenAddress, uint price) {
-        require(price > 0, "Price must be greater than 0");
-
-        token = IMinimalToken(tokenAddress);
+        token = MinimalToken(tokenAddress);
         tokenPrice = price;
         owner = msg.sender;
     }
 
     function buy() public payable {
-        require(msg.value > 0, "Send some Ether");
-
         uint amount = msg.value / tokenPrice;
         require(amount > 0, "Not enough Ether");
         require(token.balanceOf(address(this)) >= amount, "Not enough tokens");
